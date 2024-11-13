d6d7270c941b6ea2f649c5a1bb1e43b1e0612cce
pingusdelingus <alphaspammail2016@gmail.com>
2024-11-13 01:32:45 -0500

    added cleaner unix philo based code

--- bb.py
+++ bb.py
@@ -9,7 +9,7 @@ class Player:
         self.name = name
         self.hand = []
 
-# take in a player object, currentHandValue and evaluate their hand
+# take in a player object,  and evaluate their hand (sum) and returns it
 def evaluateHandBJ(Player ):
     h = Player.hand
     checkAce = 0
@@ -36,17 +36,63 @@ def evaluateHandBJ(Player ):
         return sum
     print('how tf did this code execute')
 
-
+def showHandValue(Player):
+    val = evaluateHandBJ(Player)
+    print(f"{Player.name} has a hand: {Player.hand} with value {val}")
 
 
 # deal to person
-def dealCards(person, Deck):
+def dealCard(person, Deck):
     person.hand.append(Deck.deal())
 
 def firstDeal(Player, Dealer, Deck):
     for index in range(2):
-        dealCards(Player, Deck)
-        dealCards(Dealer, Deck)
+        dealCard(Player, Deck)
+        dealCard(Dealer, Deck)
+def flipDealerCards(Player, Deck, Dealer):
+    dealerValue = evaluateHandBJ(Dealer)
+    showHandValue(Dealer)
+    if dealerValue >= 17:
+        print('dealer stands, comparing hands')
+    while(dealerValue < 17):
+        if dealerValue > 21:
+            print('dealer busts')
+        dealCard(Dealer, Deck)
+        dealerValue = evaluateHandBJ(Dealer)
+        showHandValue(Dealer)
+    
+
+human = Player('human')
+dealer = Player('dealer')
+thedeck = Deck()
+firstDeal(human, dealer, thedeck)
+showHandValue(human)
+
+
+print(f"The dealer is showing a {dealer.hand[0]}")
+
+usrInput = input("What would you like to do? (H)it |  (S)tand | (K)Surrender | or e(X)it:")
+while usrInput not in validOptions:
+    usrInput = input("What would you like to do? (H)it |  (S)tand | (K)Surrender | or e(X)it:")
+    
+if usrInput.upper() == "H":
+    dealCard(ourPlayer, ourDeck)
+elif usrInput.upper() == "S":
+    dealersum = flipDealerCards(ourPlayer, ourDeck, ourDealer)
+    compare()
+elif usrInput.upper() == "K":
+    print("Surrendering hand")
+    ourDealer.emptyHand()
+    ourPlayer.emptyHand()
+
+
+
+
+
+
+
+
+
 
 
 
