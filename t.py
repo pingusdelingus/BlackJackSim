ab38a6d29c1e18465bad6049f05eb129d43a22b6
pingusdelingus <alphaspammail2016@gmail.com>
2025-05-11 14:03:11 -0400

    organizing2

--- t.py
+++
@@ -1,8 +0,0 @@
-count = 0
-for x in range(9):  # 0 to 8
-    for y in range(9 - x):  # ensures y + x doesn't exceed 8
-        for z in range(9 - x - y):  # ensures x + y + z doesn't exceed 8
-            if 3 <= x + y + z <= 8:
-                count += 1
-
-print(count)
